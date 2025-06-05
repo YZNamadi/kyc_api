84a7855c64c9c9b974402b4cf73bccc2f8c1d3b7
YZNamadi <you@example.com>
2025-06-05 05:36:15 -0500

    fixed error

--- src/middleware/authorize.ts
+++ src/middleware/authorize.ts
@@ -3,13 +3,13 @@ import { UserRole } from '../models/User';
 import { AppError } from '../utils/errorHandler';
 
 export const authorize = (roles: UserRole[]) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
     if (!req.user) {
       throw new AppError('User not authenticated', 401);
     }
 
     if (!roles.includes(req.user.role)) {
-      throw new AppError('Not authorized to access this resource', 403);
+      throw new AppError('User not authorized', 403);
     }
 
     next();
